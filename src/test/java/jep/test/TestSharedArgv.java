ce9eefbc5b701b6b47ec8d48e8bc253e1890a36b
Ben Steffensmeier <ben.steffensmeier@gmail.com>
2025-01-19 19:42:18 -0600

    Ensure shared argv is correct length.

--- src/test/java/jep/test/TestSharedArgv.java
+++ src/test/java/jep/test/TestSharedArgv.java
@@ -38,6 +38,9 @@ public class TestSharedArgv {
             interp.eval("import logging");
             List<String> result = (List<String>) interp
                     .getValue("logging.sys.argv");
+            if (argv.length != result.size()) {
+                throw new RuntimeException("len(argv) did not match");
+            }
             for (int i = 0; i < result.size(); i++) {
                 if (!result.get(i).equals(argv[i])) {
                     throw new RuntimeException("argv[" + i + "] did not match");
